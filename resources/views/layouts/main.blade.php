133c2a9c202ded16b9c44b22036d8ccdc9bdc1ee
gilangrahmatakbar <gilangrahmatakbar213@gmail.com>
2021-10-29 01:09:27 +0700

    snow baru

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,34 +4,45 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Hello, world!</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/vendor/fontawesome-free/css/all.min.css">
+    {{-- CSS --}}
+    <link rel="stylesheet" href="/css/style.css">
+
+    <link rel="shortcut icon" href="/img/epw-logo/logo-bg-blue.png" type="image/x-icon">
+
+    <title>EPW 2022</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-warning mb-4">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-            <ul class="nav-item ms-auto navbar-nav">
-             @auth
+    {{-- Navbar --}}
+    <nav class="navbar navbar-expand-md py-0">
+      <div class="container">
+        <div class="navbar-brand flex-md-grow-1">
+          <a href="#">
+            <img src="/img/epw-logo/logo-white.png" alt="EPW Logo" width="60">
+          </a>
+        </div>
+        <div class="navbar-rainbow">
+          <div class="rainbow"></div>
+          <div class="rainbow"></div>
+          <div class="rainbow"></div>
+          <div class="rainbow"></div>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="fas fa-bars fa-lg fa-fw"></span>
+        </button>
+        <div class="collapse navbar-collapse flex-md-grow-0" id="navbarNav">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item text-center">
+              <a class="nav-link my-3" href="/">Main Website</a>
+            </li>
+            <li class="nav-item text-center">
+              <a class="nav-link my-3" href="/register">Registrasi</a>
+            </li>
+            @auth
                 <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                    Halooo, {{ auth()->user()->username }}
@@ -51,16 +62,15 @@
                 <li class="nav-item">
                     <a href="/login" class="nav-link">Login</a>
                 </li> 
-             @endauth 
-            </ul>
-          </div>
+             @endauth
+          </ul>
         </div>
+      </div>
     </nav>
-    <div class="container">
-        @yield('content')
-    </div>
+    {{-- End of Navbar --}}
 
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> 
+    @yield('content') 
+    {{-- Javascript --}}
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </body>
 </html>
