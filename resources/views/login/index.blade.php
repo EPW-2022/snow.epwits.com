09954cfcef8b044830f7ce993f475ff7171a06e5
gilangrahmatakbar <gilangrahmatakbar213@gmail.com>
2021-11-02 05:47:25 +0700

    perbaikan
    kurang yang tulisan daftar sekarang

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -17,14 +17,15 @@
             </div>
         @endif
           <div class="row align-items-center">
-            <div class="col-md-4 px-6 logo">
+            <div class="col-md-4 px-6 logo mt-1">
               <img src="/img/logosnow.png" class="login-logo d-block mx-auto" alt="">
               <img src="/svg/logosnow.svg" class="login-epw-2022 d-block mx-auto" alt="">
             </div>
-            <div class="col-md-7 px-6 user_pass"> 
-              <form action="/login" method="POST" class="login-form mx-auto me-2 px-4 mt-5 mt-md-0">
+            <div class="col-md-3"></div>
+            <div class="col-md-5 px-6 user_pass mx-auto"> 
+              <form action="/login" method="POST" class="login-form px-4 mt-5 mt-md-0 mx-auto">
                 @csrf
-                <div class="mb-4 ">
+                <div class="mb-4">
                   <label for="username" class="form-label">Username</label>
                   <input class="form-control @error('username') is-invalid @enderror" type="text" id="username" name="username" placeholder="Username" autofocus tabindex="1" autocomplete="on" required>
                 </div>
@@ -33,7 +34,7 @@
                   <input class="form-control @error('password') is-invalid @enderror" type="password" id="password" name="password" placeholder="Password" tabindex="2" required>
                 </div>
                 <button type="submit" class="btn login-button w-100" tabindex="3">Log In</button>
-                <p class="login-register text-center mt-3" tabindex="4">Belum punya akun? <a href="/register">Daftar sekarang!</a></p>
+                <p class="login-register text-center mt-3 mx-auto" tabindex="4">Belum punya akun? <a href="/register">Daftar sekarang!</a></p>
               </form>
             </div>
           </div>
