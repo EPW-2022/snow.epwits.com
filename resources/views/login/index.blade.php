b19b6a7757ba5662afa7c79829326bf7e4396a9e
hicthhatch10 <abinaufal40@gmail.com>
2021-11-01 07:43:42 +0700

    pembenaran page login
    problem fixed dengan ganti class dan id selector scss

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
 @section('content')
-<section id="loginForm">
+<section class="loginm">
         <div class="container d-flex justify-content-center flex-column py-5">
         @if (session()->has('success'))
             <div class="alert alert-success alert-dismissible fade show mt-3" role="alert">
@@ -17,23 +17,23 @@
             </div>
         @endif
           <div class="row align-items-center">
-            <div class="col-md-4 px-6">
+            <div class="col-md-4 px-6 logo">
               <img src="/img/logosnow.png" class="login-logo d-block mx-auto" alt="">
               <img src="/svg/logosnow.svg" class="login-epw-2022 d-block mx-auto" alt="">
             </div>
-            <div class="col-md-7 px-6"> 
+            <div class="col-md-7 px-6 user_pass"> 
               <form action="/login" method="POST" class="login-form mx-auto me-2 px-4 mt-5 mt-md-0">
                 @csrf
-                <div class="mb-4">
-                  <label for="username" class="form-label">Username</label>
-                  <input class="form-control @error('username') is-invalid @enderror" type="text" id="username" name="username" placeholder="Username" autofocus tabindex="1" autocomplete="on" required>
+                <div class="mb-4 ">
+                  <label for="username" class="form-label">Alamat email</label>
+                  <input class="form-control @error('username') is-invalid @enderror" type="text" id="username" name="username" placeholder="Alamat email" autofocus tabindex="1" autocomplete="on" required>
                 </div>
                 <div class="mb-4">
                   <label for="password" class="form-label">Password</label>
                   <input class="form-control @error('password') is-invalid @enderror" type="password" id="password" name="password" placeholder="Password" tabindex="2" required>
                 </div>
                 <button type="submit" class="btn login-button w-100" tabindex="3">Log In</button>
-                <p class="login-register text-center mt-3" tabindex="4">Belum mendaftar? </p><p><a href="/register">Daftar sekarang!</a></p>
+                <p class="login-register text-center mt-3" tabindex="4">Belum punya akun? <a href="/register">Daftar sekarang!</a></p>
               </form>
             </div>
           </div>
